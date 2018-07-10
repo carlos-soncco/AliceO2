2a985c978b725f31e58c71364851c4387ba843b5
Carlos-Soncco <csonccom@cern.ch>
2018-07-09 19:02:57 -0500

    front6.4 mod

--- Detectors/ITSMFT/MFT/base/src/HalfSupport.cxx
+++ Detectors/ITSMFT/MFT/base/src/HalfSupport.cxx
@@ -981,7 +981,7 @@ TGeoVolumeAssembly* HalfSupport::createHalfSupport(Int_t half)
  //-     HalfSupportVolume->AddNode(stair,1,new TGeoTranslation(0,0,-80)); //+0.675z
  //-     HalfSupportVolume->AddNode(base,2,new TGeoTranslation(0,0,-80));
  //-     HalfSupportVolume->AddNode(rail_L_R,3,new TGeoTranslation(0,0,-80));//0.6 R&L (0,-6.9,-0.5-80)
-      HalfSupportVolume->AddNode(Fra_front,4,new TGeoTranslation(0,0,10));//-7.1,-16.2,32.5-80
+      HalfSupportVolume->AddNode(Fra_front,4,new TGeoTranslation(0,0,-80));//-7.1,-16.2,32.5-80
  
     //  HalfSupportVolume->AddNode(Fra_front,5,new TGeoTranslation(0,0,-80));//-7.1,-16.2,32.5-80
      
